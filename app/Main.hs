3d771e01c4c40274c06624686b01dc0b6b9a8bab
Guillaume Collic <gcollic@gmail.com>
2020-01-25 17:43:10 +0100

    Working skeleton

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
 
 import Lib
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as Tio
+import System.Exit
+import System.Environment
 
 main :: IO ()
-main = someFunc
+main = getArgs >>= parse >>= parseGedcom -- >>= print
+
+parse :: [String] -> IO Text
+parse args 
+    | ["-h"] <- args = usage >> exitSuccess
+    | (fs:_) <- args = Tio.readFile (fs :: FilePath)
+    | otherwise  = usage >> exitWith (ExitFailure 1)
+    where usage = Tio.putStrLn "Usage: familytree [-h] [file ..]"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- {-# LANGUAGE RecordWildCards   #-}
+
 module Lib
-    ( someFunc
+    ( parseGedcom,
+    gedcom
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Applicative
+import Control.Monad
+import Data.Text (Text)
+import Data.Void
+import Text.Megaparsec hiding (State)
+import Text.Megaparsec.Char
+import qualified Data.Text as T
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void Text
+
+parseGedcom :: Text -> IO () 
+parseGedcom = parseTest gedcom
+
+gedcom :: Parser Text
+gedcom = string "data"
+  <|> string "file"
+  <|> string "ftp"
+  <|> string "http"
+  <|> string "https"
+  <|> string "irc"
+  <|> string "mailto"
